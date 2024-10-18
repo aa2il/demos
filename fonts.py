2504675b6861e24de9c6f631b260435f120e3d5b
aa2il <jattili@yahoo.com>
2024-10-18 14:33:11 -0700

    Updates

--- fonts.py
+++ fonts.py
@@ -1,6 +1,74 @@
 #! /usr/bin/python3
-
+##############################################################################
+#
 # Script to explore fonts available
+#
+# By default, conda does not include very many fonts for tk and therefore the
+# tk guis look ugly.  Do this when creating the sandbox to avoid this:
+#
+# conda create -y --prefix "conda_env" -c conda-forge "python==3.12.*" "tk[build=xft_*]"
+#
+# This can be used to fix an existing sandbox:
+#
+# conda install --prefix "conda_env" -c conda-forge tk=*=xft_* 
+#
+# In my case, conda_env is ~/miniconda3/envs/aa2il_12 or whatever
+#
+##############################################################################
+
+from tkinter import *
+from tkinter import font
+import sys
+
+root = Tk()
+root.title('Font Families')
+fonts=list(font.families())
+fonts.sort()
+
+print('\nAvailable fonts:',fonts)
+print('\nDefault font:',font.nametofont('TkTextFont').actual())
+sys.exit(0)
+
+size=24
+
+def populate(frame):
+    '''Put in the fonts'''
+    listnumber = 1
+    for i, item in enumerate(fonts):
+        print('Loading item',i,'of',len(fonts),':',item,'...')
+        label = "listlabel" + str(listnumber)
+        label = Label(frame,text=item,font=(item, size))
+        label.grid(row=i)
+        label.bind("<Button-1>",lambda e,item=item:copy_to_clipboard(item))
+        listnumber += 1
+
+def copy_to_clipboard(item):
+    root.clipboard_clear()
+    root.clipboard_append("font=('" + item.lstrip('@') + "', 12)")
+
+def onFrameConfigure(canvas):
+    '''Reset the scroll region to encompass the inner frame'''
+    canvas.configure(scrollregion=canvas.bbox("all"))
+
+canvas = Canvas(root, borderwidth=0, background="#ffffff")
+frame = Frame(canvas, background="#ffffff")
+vsb = Scrollbar(root, orient="vertical", command=canvas.yview)
+canvas.configure(yscrollcommand=vsb.set)
+
+vsb.pack(side="right", fill="y")
+canvas.pack(side="left", fill="both", expand=True)
+canvas.create_window((4,4), window=frame, anchor="nw")
+
+frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+
+populate(frame)
+
+root.mainloop()
+
+
+sys.exit(0)
+
+
 
 from tkinter import Tk, font
 root = Tk()
---
+++ hello_qt.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+# A very simple Hello World using Qt
+
+import sys
+try:
+   from PyQt6.QtWidgets import *       # Exists fine
+   #from PySide6.QtWidgets import *    # Hangs on exit --> bug in PySide6
+   from PyQt6.QtCore import Qt,qVersion
+   from PyQt6.QtGui import QFont
+except ImportError:
+   #from PyQt4.QtGui import *          # Very old - don't bother anymore
+   from PyQt5.QtWidgets import *
+   from PyQt5.QtCore import Qt,qVersion
+   from PyQt5.QtGui import QFont
+
+def window():
+   app = QApplication(sys.argv)
+   w = QWidget()
+
+   print(QFont.__dict__)
+   print(dir(QFont))
+   font=QFont('Arial',20)
+   font.setBold(True)
+   
+   b = QLabel(w,font=font)
+   b.setText("Hello World! - PyQt Gui")
+
+   w.show()
+   sys.exit(app.exec())
+	
+if __name__ == '__main__':
+   msg="Hello World! - QT Version ="+qVersion()
+   print(msg)
+   window()
