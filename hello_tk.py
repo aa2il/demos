639eda1fbd5bd0da596fd9f62d456db7d2219410
aa2il <jattili@yahoo.com>
2025-11-18 11:32:41 -0800

    Updates

--- hello_tk.py
+++ hello_tk.py
@@ -1,12 +1,16 @@
-#! /usr/bin/python3
+#!/usr/bin/env -S uv run --script
+
+#OLD: ! /usr/bin/python3
 
 # A very simple Hello World using Tk.  Also used to figure out selection
 
 import sys
 if sys.version_info[0]==3:
     from tkinter import *
+    import tkinter.font
 else:
     from Tkinter import *
+    import tkFont
 import time
 
 ###############################################################################
@@ -22,14 +26,20 @@ def Selection(evt):
     # can get the selection
     root.after(10,lambda: try_again(evt) )
 
+print('version=',sys.version_info)
+    
 root = Tk()
 msg="Hello World! - Tk Version"
 print(msg)
 
-w = Label(root, text=msg)
+FAMILY="monospace"
+SIZE=28
+fnt =  tkinter.font.Font(family=FAMILY,size=SIZE, weight="bold")
+
+w = Label(root, text=msg, font=fnt)
 w.pack()
 
-e = Entry(root,fg='blue',selectbackground='white')
+e = Entry(root,fg='blue',selectbackground='white', font=fnt)
 e.pack()
 e.insert(0,"This is an Entry Box")
 
